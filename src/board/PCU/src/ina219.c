c52f2223be122234225aaf5030c124da78f760d1
snork2 <strange.snork@gmail.com>
2019-04-27 00:18:29 +0300

    Реализация I2C на прерываниях не выдерживает критики. В остальном скорее работает, чем нет

--- src/board/PCU/src/ina219.c
+++ src/board/PCU/src/ina219.c
@@ -23,11 +23,11 @@
 
 
 #define INA219_SET_BITS(reg_data, bitname, data) \
-                ((reg_data & ~(bitname##_MSK)) | \
-                ((data << bitname##_POS) & bitname##_MSK))
+				((reg_data & ~(bitname##_MSK)) | \
+				((data << bitname##_POS) & bitname##_MSK))
 
 #define INA219_GET_BITS(reg_data, bitname)  ((reg_data & (bitname##_MSK)) >> \
-                            (bitname##_POS))
+							(bitname##_POS))
 
 #define INA219_CFGREG_ADDR         (0x00)
 
@@ -81,12 +81,12 @@
 //! Запись одного регистра ины в I2C шину
 static int _write_reg(ina219_t * device, uint8_t reg_addr, uint16_t reg_value)
 {
-    int error;
-    // помним, что ина работает в big_endian, а мы в little
-    uint8_t package[] = {reg_addr, reg_value >> 8, reg_value & 0xFF };
+	int error;
+	// помним, что ина работает в big_endian, а мы в little
+	uint8_t package[] = {reg_addr, reg_value >> 8, reg_value & 0xFF };
 
-    error = i2c_write(device->bus, device->address, package, sizeof(package));
-    return error;
+	error = i2c_write(device->bus, device->address, package, sizeof(package));
+	return error;
 }
 
 
@@ -94,62 +94,62 @@ static int _write_reg(ina219_t * device, uint8_t reg_addr, uint16_t reg_value)
 /*! К сожалению ина не умеет читать несколько регистров за раз */
 static int _read_reg(ina219_t * device, uint8_t reg_addr, uint16_t * reg_value)
 {
-    int error;
-    error = i2c_write(device->bus, device->address, &reg_addr, sizeof(reg_addr));
-    if (error)
-        return error;
+	int error;
+	error = i2c_write(device->bus, device->address, &reg_addr, sizeof(reg_addr));
+	if (error)
+		return error;
 
-    uint16_t raw_reg_value;
-    error = i2c_read(device->bus, device->address, &raw_reg_value, sizeof(raw_reg_value));
+	uint16_t raw_reg_value;
+	error = i2c_read(device->bus, device->address, &raw_reg_value, sizeof(raw_reg_value));
 
-    // помним, что ина работает в big_endian, а мы в little
-    *reg_value = (raw_reg_value >> 8) | (raw_reg_value << 8);
-    *reg_value = raw_reg_value;
-    return error;
+	// помним, что ина работает в big_endian, а мы в little
+	*reg_value = (raw_reg_value << 8) | (raw_reg_value >> 8);
+	*reg_value = raw_reg_value;
+	return error;
 }
 
 
 //! Построение значения калибровочного регистра из его базовых значений
 static uint16_t _make_cal_reg(ina219_float_t current_lsb, ina219_float_t shunt_r)
 {
-    // Если что-то задано по нулям, то калибровки не будет
-    if (current_lsb == INA219_FLOAT(0.0) || shunt_r == INA219_FLOAT(0.0))
-        return 0x00;
+	// Если что-то задано по нулям, то калибровки не будет
+	if (current_lsb == INA219_FLOAT(0.0) || shunt_r == INA219_FLOAT(0.0))
+		return 0x00;
 
-    // волшебные цифры из даташита
-    const ina219_float_t calib = INA219_FLOAT(0.04096)/(current_lsb * shunt_r);
-    return ((uint16_t)calib) << 1;
-    // << 1 так как нулевой бит регистра резервирован, а фактическое значение идет с бита 1
+	// волшебные цифры из даташита
+	const ina219_float_t calib = INA219_FLOAT(0.04096)/(current_lsb * shunt_r);
+	return ((uint16_t)calib) << 1;
+	// << 1 так как нулевой бит регистра резервирован, а фактическое значение идет с бита 1
 }
 
 
 //! Построение значения cfg регистра из структуры конфигурации
 static uint16_t _make_cfg_reg(const ina219_cfg_t * cfg)
 {
-    uint16_t retval = 0;
+	uint16_t retval = 0;
 
-    retval = INA219_SET_BITS(retval, INA219_CFGREG__BRNG, cfg->bus_range);
-    retval = INA219_SET_BITS(retval, INA219_CFGREG__PG, cfg->shunt_range);
-    retval = INA219_SET_BITS(retval, INA219_CFGREG__BADC, cfg->bus_res);
-    retval = INA219_SET_BITS(retval, INA219_CFGREG__SADC, cfg->shunt_res);
-    retval = INA219_SET_BITS(retval, INA219_CFGREG__MODE, cfg->mode);
+	retval = INA219_SET_BITS(retval, INA219_CFGREG__BRNG, cfg->bus_range);
+	retval = INA219_SET_BITS(retval, INA219_CFGREG__PG, cfg->shunt_range);
+	retval = INA219_SET_BITS(retval, INA219_CFGREG__BADC, cfg->bus_res);
+	retval = INA219_SET_BITS(retval, INA219_CFGREG__SADC, cfg->shunt_res);
+	retval = INA219_SET_BITS(retval, INA219_CFGREG__MODE, cfg->mode);
 
-    return retval;
+	return retval;
 }
 
 
 //! нарезка значения "первичных" регистров на "исходные" данные
 inline static void _slice_prim_data(const uint16_t * primregs, ina219_primary_data_t * primdata)
 {
-    // тут нужно хитро. нужно скопировать бфайты из uint16_t так, чтобы они воспринимались
-    // как int16_t
-    uint16_t * const uintptr = (uint16_t *)&primdata->shuntv;
-    *uintptr = primregs[0];
+	// тут нужно хитро. нужно скопировать бфайты из uint16_t так, чтобы они воспринимались
+	// как int16_t
+	uint16_t * const uintptr = (uint16_t *)&primdata->shuntv;
+	*uintptr = primregs[0];
 
-    const uint16_t busvreg = primregs[1];
-    primdata->busv = INA219_GET_BITS(busvreg , INA219_BUSVREG__BUSV);
-    primdata->cnvr = INA219_GET_BITS(busvreg , INA219_BUSVREG__CNVR);
-    primdata->ovf = INA219_GET_BITS(busvreg, INA219_BUSVREG__OVF);
+	const uint16_t busvreg = primregs[1];
+	primdata->busv = INA219_GET_BITS(busvreg , INA219_BUSVREG__BUSV);
+	primdata->cnvr = INA219_GET_BITS(busvreg , INA219_BUSVREG__CNVR);
+	primdata->ovf = INA219_GET_BITS(busvreg, INA219_BUSVREG__OVF);
 
 }
 
@@ -157,151 +157,151 @@ inline static void _slice_prim_data(const uint16_t * primregs, ina219_primary_da
 //! нарезка значений "расчетных" регистров на "расчетные" данные
 inline static void _slice_sec_data(const uint16_t * secregs, ina219_secondary_data_t * secdata)
 {
-    // все очень просто
-    secdata->power = secregs[0];
+	// все очень просто
+	secdata->power = secregs[0];
 
-    // тут нужно хитро. нужно скопировать бфайты из uint16_t так, чтобы они воспринимались
-    // как int16_t
-    uint16_t * const uintptr = (uint16_t *)&secdata->current;
-    *uintptr = secregs[1];
+	// тут нужно хитро. нужно скопировать бфайты из uint16_t так, чтобы они воспринимались
+	// как int16_t
+	uint16_t * const uintptr = (uint16_t *)&secdata->current;
+	*uintptr = secregs[1];
 }
 
 
 
 void ina219_load_default_cfg(ina219_cfg_t * cfg)
 {
-    cfg->bus_range = INA219_BUS_RANGE_32V;
-    cfg->bus_res = INA219_ADC_RES_12_BIT_OVS_1;
+	cfg->bus_range = INA219_BUS_RANGE_32V;
+	cfg->bus_res = INA219_ADC_RES_12_BIT_OVS_1;
 
-    cfg->shunt_range = INA219_SHUNT_RANGE_320MV;
-    cfg->shunt_res = INA219_ADC_RES_12_BIT_OVS_1;
+	cfg->shunt_range = INA219_SHUNT_RANGE_320MV;
+	cfg->shunt_res = INA219_ADC_RES_12_BIT_OVS_1;
 
-    cfg->mode = INA219_MODE_SHUNT_AND_BUS_CONT;
+	cfg->mode = INA219_MODE_SHUNT_AND_BUS_CONT;
 
-    cfg->shunt_r = 0.0f;
-    cfg->current_lsb = 0.0f;
+	cfg->shunt_r = 0.0f;
+	cfg->current_lsb = 0.0f;
 }
 
 
 void ina219_init(ina219_t * device, I2C_TypeDef * bus, uint8_t i2c_addr)
 {
-    device->bus = bus;
-    device->address = i2c_addr;
-    ina219_load_default_cfg(&device->cfg);
+	device->bus = bus;
+	device->address = i2c_addr;
+	ina219_load_default_cfg(&device->cfg);
 }
 
 
 void ina219_deinit(ina219_t * device)
 {
-    (void)device;
-    // нечего делать..,
+	(void)device;
+	// нечего делать..,
 }
 
 
 int ina219_sw_reset(ina219_t * device)
 {
-    uint16_t cfgreg = 0;
-    cfgreg = INA219_SET_BITS(cfgreg, INA219_CFGREG__RESET, 1);
-    return _write_reg(device, INA219_CFGREG_ADDR, cfgreg);
+	uint16_t cfgreg = 0;
+	cfgreg = INA219_SET_BITS(cfgreg, INA219_CFGREG__RESET, 1);
+	return _write_reg(device, INA219_CFGREG_ADDR, cfgreg);
 }
 
 
 int ina219_set_cfg(ina219_t * device, const ina219_cfg_t * cfg)
 {
-    uint16_t cfgreg = _make_cfg_reg(cfg);
-    int error = _write_reg(device, INA219_CFGREG_ADDR, cfgreg);
-    if (error)
-        return error;
+	uint16_t cfgreg = _make_cfg_reg(cfg);
+	int error = _write_reg(device, INA219_CFGREG_ADDR, cfgreg);
+	if (error)
+		return error;
 
-    device->cfg = *cfg;
-    // FIXME: не все копировать
+	device->cfg = *cfg;
+	// FIXME: не все копировать
 
-    uint16_t calreg = _make_cal_reg(cfg->current_lsb, cfg->shunt_r);
-    error = _write_reg(device, INA219_CALREG_ADDR, calreg);
-    if (error)
-        return error;
+	uint16_t calreg = _make_cal_reg(cfg->current_lsb, cfg->shunt_r);
+	error = _write_reg(device, INA219_CALREG_ADDR, calreg);
+	if (error)
+		return error;
 
 
-    device->cfg.current_lsb = cfg->current_lsb;
-    device->cfg.shunt_r = cfg->shunt_r;
+	device->cfg.current_lsb = cfg->current_lsb;
+	device->cfg.shunt_r = cfg->shunt_r;
 
-    return 0;
+	return 0;
 }
 
 
 const ina219_cfg_t * ina219_get_cfg(ina219_t * device)
 {
-    return &device->cfg;
+	return &device->cfg;
 }
 
 
 int ina219_set_mode(ina219_t * device, const ina219_mode_t mode)
 {
-    // запоминаем старый режим, на случай если что-то пойдет не так
-    const ina219_mode_t old_mode = device->cfg.mode;
-    // пробуем обновить режим в дескрипторе
-    device->cfg.mode = mode;
+	// запоминаем старый режим, на случай если что-то пойдет не так
+	const ina219_mode_t old_mode = device->cfg.mode;
+	// пробуем обновить режим в дескрипторе
+	device->cfg.mode = mode;
 
-    // пробуем зашить новый конфиг регистр
-    uint16_t cfgreg = _make_cfg_reg(&device->cfg);
-    int error = _write_reg(device, INA219_CFGREG_ADDR, cfgreg);
+	// пробуем зашить новый конфиг регистр
+	uint16_t cfgreg = _make_cfg_reg(&device->cfg);
+	int error = _write_reg(device, INA219_CFGREG_ADDR, cfgreg);
 
-    // Если что-то пошло не так - восстанавливаем старый режим
-    // он наиболее соответствует действительности
-    if (error)
-        device->cfg.mode = old_mode;
+	// Если что-то пошло не так - восстанавливаем старый режим
+	// он наиболее соответствует действительности
+	if (error)
+		device->cfg.mode = old_mode;
 
-    return error;
+	return error;
 }
 
 
 int ina219_read_primary(ina219_t * device, ina219_primary_data_t * data)
 {
-    uint16_t raw_reg_data[2];
-    int error = _read_reg(device, INA219_SHUNTVREG_ADDR, &raw_reg_data[0]);
-    if (error)
-        return error;
+	uint16_t raw_reg_data[2];
+	int error = _read_reg(device, INA219_SHUNTVREG_ADDR, &raw_reg_data[0]);
+	if (error)
+		return error;
 
-    error = _read_reg(device, INA219_BUSVREG_ADDR, &raw_reg_data[1]);
-    if (error)
-        return error;
+	error = _read_reg(device, INA219_BUSVREG_ADDR, &raw_reg_data[1]);
+	if (error)
+		return error;
 
 
-    _slice_prim_data(raw_reg_data, data);
-    return 0;
+	_slice_prim_data(raw_reg_data, data);
+	return 0;
 }
 
 
 int ina219_read_secondary(ina219_t * device, ina219_secondary_data_t * data)
 {
-    uint16_t raw_reg_data[2];
-    int error = _read_reg(device, INA219_POWERREG_ADDR, &raw_reg_data[0]);
-    if (error)
-        return error;
+	uint16_t raw_reg_data[2];
+	int error = _read_reg(device, INA219_POWERREG_ADDR, &raw_reg_data[0]);
+	if (error)
+		return error;
 
-    error = _read_reg(device, INA219_CURRENTREG_ADDR, &raw_reg_data[1]);
-        if (error)
-            return error;
+	error = _read_reg(device, INA219_CURRENTREG_ADDR, &raw_reg_data[1]);
+		if (error)
+			return error;
 
 
-    _slice_sec_data(raw_reg_data, data);
-    return 0;
+	_slice_sec_data(raw_reg_data, data);
+	return 0;
 }
 
 
 int ina219_read_all(ina219_t * device, ina219_primary_data_t * prim_data,
-        ina219_secondary_data_t * secondary_data)
+		ina219_secondary_data_t * secondary_data)
 {
-    int error = ina219_read_primary(device, prim_data);
-    if (error)
-        return error;
+	int error = ina219_read_primary(device, prim_data);
+	if (error)
+		return error;
 
-    error = ina219_read_secondary(device, secondary_data);
-    return error;
+	error = ina219_read_secondary(device, secondary_data);
+	return error;
 }
 
 int ina219_read_reg(ina219_t * device, uint8_t regaddr, uint16_t * regdata)
 {
-    return _read_reg(device, regaddr, regdata);
+	return _read_reg(device, regaddr, regdata);
 }
 
--- src/board/PCU/src/main.c
+++ src/board/PCU/src/main.c
@@ -11,72 +11,67 @@
 
 int main()
 {
-    i2c_init_bus1();
+	i2c_init_bus1();
 
-    int error = 0;
-    ina219_t ina;
-    ina219_init(&ina, I2C1, INA219_I2CADDR_A1_GND_A0_GND);
-    error = ina219_sw_reset(&ina);
-    trace_printf("reset error %d\n", error);
-    // подождем после резета
-    for (volatile int x = 0; x < 1000000; x++) {}
+	int error = 0;
+	ina219_t ina;
+	ina219_init(&ina, I2C1, INA219_I2CADDR_A1_GND_A0_GND);
+	error = ina219_sw_reset(&ina);
+	trace_printf("reset error: %d\n", error);
+	// подождем после резета
+	for (volatile int x = 0; x < 1000000; x++) {}
 
-    uint16_t cfgreg;
-    ina219_read_reg(&ina, 0x00, &cfgreg);
-    trace_printf("cfgreg:0x%04" PRIX16 "\n", cfgreg);
-    for (volatile int x = 0; x < 5000000; x++) {}
+	uint16_t cfgreg;
+	error = ina219_read_reg(&ina, 0x00, &cfgreg);
+	trace_printf("initial cfgreg: 0x%04"PRIX16", error = %d\n", cfgreg, error);
 
-    ina219_cfg_t ina_cfg;
-    ina_cfg.bus_range = INA219_BUS_RANGE_16V;
-    ina_cfg.bus_res = INA219_ADC_RES_12_BIT_OVS_1;
-    ina_cfg.shunt_range = INA219_SHUNT_RANGE_80MV;
-    ina_cfg.shunt_res = INA219_ADC_RES_12_BIT_OVS_1;
-    ina_cfg.mode = INA219_MODE_SHUNT_AND_BUS_CONT;
+	ina219_cfg_t ina_cfg;
+	ina_cfg.bus_range = INA219_BUS_RANGE_32V;
+	ina_cfg.bus_res = INA219_ADC_RES_12_BIT_OVS_1;
+	ina_cfg.shunt_range = INA219_SHUNT_RANGE_320MV;
+	ina_cfg.shunt_res = INA219_ADC_RES_12_BIT_OVS_1;
+	ina_cfg.mode = INA219_MODE_SHUNT_AND_BUS_CONT;
 
+	ina_cfg.shunt_r = 0.1f; // 100 миллиом
+	ina_cfg.current_lsb = 1.f/0x8000; // считаем из расчета на 1 ампер максимум
+	error = ina219_set_cfg(&ina, &ina_cfg);
+	trace_printf("set_cfg error: %d\n", error);
 
-    ina_cfg.shunt_r = 0.1f; // 100 миллиом
-    ina_cfg.current_lsb = 1.f/0x8000; // считаем из расчета на 1 ампер максимум
-    //error = ina219_set_cfg(&ina, &ina_cfg);
-    trace_printf("set_cfg error %d\n", error);
-
-
-    while(1)
-    {
-        ina219_primary_data_t prim_data;
-
-        error = ina219_read_primary(&ina, &prim_data);
-        int32_t shunt_mv = prim_data.shuntv*INA219_SHUNTV_LSB_MKV/1000;
-        int32_t bus_mv = prim_data.busv*INA219_BUSV_LSB_MV;
-        trace_printf("error %d shunt-raw:%" PRId16 " bus-raw:%" PRIu16 " shunt:%d bus:%d: cnvr:%d ovf:%d\n",
-                error,
-                prim_data.shuntv, prim_data.busv,
-                shunt_mv, bus_mv,
-                prim_data.cnvr, prim_data.ovf
-        );
-
-        uint16_t cfgreg;
-        ina219_read_reg(&ina, 0x00, &cfgreg);
-        trace_printf("cfgreg:0x%04" PRIX16 "\n", cfgreg);
-        for (volatile int x = 0; x < 5000000; x++) {}
+	error = ina219_read_reg(&ina, 0x00, &cfgreg);
+	trace_printf("setted cfgreg: 0x%04"PRIX16" , error: %d\n", cfgreg, error);
 
 
-        /*
-        ina219_primary_data_t prim_data;
-        ina219_secondary_data_t sec_data;
+	while(1)
+	{
+		ina219_primary_data_t prim_data;
+		error = ina219_read_primary(&ina, &prim_data);
+		int32_t shunt_mv = prim_data.shuntv*INA219_SHUNTV_LSB_MKV/1000;
+		int32_t bus_mv = prim_data.busv*INA219_BUSV_LSB_MV;
+		trace_printf("error %d shunt-raw:%"PRId16" bus-raw:%"PRIu16" shunt:%d bus:%d: cnvr:%d ovf:%d\n",
+				error,
+				prim_data.shuntv, prim_data.busv,
+				shunt_mv, bus_mv,
+				prim_data.cnvr, prim_data.ovf
+		);
+		for (volatile int x = 0; x < 5000000; x++) {}
 
-        error = ina219_read_all(&ina, &prim_data, &sec_data);
-        trace_printf("read_all error %d\n", error);
+		/*
+		ina219_primary_data_t prim_data;
+		ina219_secondary_data_t sec_data;
 
-        trace_printf("prim_data: %" PRId16 " %" PRIu16 " %d %d\n",
-                prim_data.shuntv*INA219_SHUNTV_LSB_MV, prim_data.busv*INA219_BUSV_LSB_MV,
-                prim_data.cnvr, prim_data.ovf
-        );
+		error = ina219_read_all(&ina, &prim_data, &sec_data);
+		trace_printf("read_all error %d\n", error);
 
-        trace_printf("sec_data: %" PRIu16 " %" PRIi16 "\n",
-                sec_data.power, sec_data.current
-        );
-        */
-    }
+		trace_printf("prim_data: %" PRId16 " %" PRIu16 " %d %d\n",
+				prim_data.shuntv*INA219_SHUNTV_LSB_MV, prim_data.busv*INA219_BUSV_LSB_MV,
+				prim_data.cnvr, prim_data.ovf
+		);
 
-    return 0;
+		trace_printf("sec_data: %" PRIu16 " %" PRIi16 "\n",
+				sec_data.power, sec_data.current
+		);
+		*/
+	}
+
+	return 0;
 }
