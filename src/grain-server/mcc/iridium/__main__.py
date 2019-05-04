9121597e8f7cb814c97cb7152992e1edfe6edc77
snork2 <strange.snork@gmail.com>
2019-05-04 21:44:48 +0300

    Мелкие украшательства

--- src/grain-server/mcc/iridium/__main__.py
+++ src/grain-server/mcc/iridium/__main__.py
@@ -9,10 +9,11 @@ class ReqHandler(BaseRequestHandler):
     def handle(self):
         pass
 
+
 server = SBDServiceServer(
     server_address=("0.0.0.0", 1221),
     request_handler_cls=ReqHandler,
-    send_ack=True
+    send_ack=True,
 )
 
 server.serve_forever()
