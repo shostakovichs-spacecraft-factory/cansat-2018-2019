4ba9ac75be063be31365e641dad01e25c35bffe9
snork2 <strange.snork@gmail.com>
2019-05-07 21:32:17 +0300

    Что-то начинает прояснятся в структуре драйвера

--- research/iridium/iridium-9602-client/src/iridium9602.c
+++ research/iridium/iridium-9602-client/src/iridium9602.c
@@ -7,8 +7,10 @@
 
 #include "iridium9602.h"
 
+#include <string.h>
 #include <assert.h>
 #include <printf.h>
+#include <errno.h>
 
 
 //! Читает сколько есть полученных байт из приёмного буфера уарта связанного с модемом
@@ -26,6 +28,24 @@ static int _write_some(ir9602_t * device, const void * buffer, int buffer_size)
 }
 
 
+//! Контрольная сумма для протокола SBD
+static uint16_t _sbd_checksum(const void * data_, int datasize)
+{
+	const uint8_t * data = (const uint8_t*)data_;
+	uint16_t sum = 0;
+	for (int i = 0; i < datasize; i++)
+		sum += data[i];
+
+	return sum;
+}
+
+
+static uint16_t _swap_endian(uint16_t value)
+{
+	return (value >> 8) | (value << 8);
+}
+
+
 void ir9602_init(ir9602_t * device, ir9602_uart_read_t uart_read, ir9602_uart_write_t uart_write,
 		void * rwarg
 )
@@ -37,11 +57,124 @@ void ir9602_init(ir9602_t * device, ir9602_uart_read_t uart_read, ir9602_uart_wr
 	device->cmd_size = 0;
 	device->cmd_carret = 0;
 
-	device->mo_msg_size = 0;
-	device->mo_msg_carret = 0;
+	device->data_size = 0;
+	device->data_carret = 0;
 
-	device->mt_msg_size = 0;
-	device->mt_msg_carret = 0;
+	device->state = IR9602_STATE_IDLE;
 }
+
+
+int ir9602_cmd_at(ir9602_t * device)
+{
+	if (device->state != IR9602_STATE_IDLE)
+		return -EBUSY;
+
+	device->cmd.code = IR9602_CMD_AT;
+	ir9602_serialize_command(device->cmd_buffer, IR9602_CMD_BUFFER_SIZE, &device->cmd);
+
+	device->state = IR9602_STATE_CMD;
+	return 0;
+}
+
+
+int ir9602_cmd_sbdwb(ir9602_t * device, const void * buffer, int buffer_size)
+{
+	if (device->state != IR9602_STATE_IDLE)
+		return -EBUSY;
+
+	// проверяем что у нас все влезает с размером и контрольной суммой
+	if (sizeof(uint16_t) + buffer_size + sizeof(uint16_t) > IR9602_DATA_BUFFER_SIZE)
+		return -EOVERFLOW;
+
+	// копируем само сообщение
+	memcpy(
+			device->data_buffer,
+			buffer,
+			buffer_size
+	);
+
+	// и затем контрольную сумму
+	uint16_t checksum_word = _swap_endian(_sbd_checksum(buffer, buffer_size));
+	memcpy(
+			device->data_buffer + buffer_size,
+			&checksum_word,
+			sizeof(checksum_word)
+	);
+
+	// Запоминаем сколько данных у нас в буфере
+	device->data_size = buffer_size + sizeof(checksum_word);
+
+	// Готовим команду к отправке
+	device->cmd.code = IR9602_CMD_SBDWB;
+	device->cmd.arg.sbdwb.message_length = buffer_size;
+	ir9602_serialize_command(device->cmd_buffer, IR9602_CMD_BUFFER_SIZE, &device->cmd);
+	device->state = IR9602_STATE_CMD;
+
+	return buffer_size;
+}
+
+
+int ir9602_cmd_sbdrb(ir9602_t * device)
+{
+	// Готовим команду к отправке
+	device->cmd.code = IR9602_CMD_SBDRB;
+	ir9602_serialize_command(device->cmd_buffer, IR9602_CMD_BUFFER_SIZE, &device->cmd);
+	device->state = IR9602_STATE_CMD;
+
+	return 0;
+}
+
+
+int ir9602_get_mt_data(ir9602_t * device, void * buffer, int buffer_size)
+{
+	// Если мы сейчас читаем сообщение из модема, то читать его тут конечно же нельзя
+	if (device->state == IR9602_STATE_DATA_READ)
+		return -EBUSY;
+
+	// Если у нас нет ни байтика кроме размера сообщения и контрольной суммы
+	// то сразу сливаемся
+	if (device->data_size < sizeof(uint16_t) + 1 + sizeof(uint16_t))
+		return 0;
+
+	// Мы влезаем в выходной буфер?
+	const uint16_t payload_size = device->data_size - sizeof(uint16_t) - sizeof(uint16_t);
+	if (payload_size > buffer_size)
+		return payload_size;
+
+	// Проверим контрольную сумму?
+	uint16_t calculated_checksum = _sbd_checksum(
+			device->data_buffer + sizeof(uint16_t),
+			payload_size
+	);
+
+	uint16_t inbuffer_checksum;
+	memcpy(
+			&inbuffer_checksum,
+			device->data_buffer + sizeof(uint16_t) + payload_size,
+			sizeof(uint16_t)
+	);
+
+	if (_swap_endian(inbuffer_checksum) != calculated_checksum)
+		return -EBADMSG;
+
+	// Копируем в выходной буфер
+	memcpy(
+		buffer,
+		device->data_buffer + sizeof(uint16_t),
+		payload_size
+	);
+
+	return payload_size;
+}
+
+
+int ir9602_poll_one(ir9602_t * device)
+{
+
+
+	return 0;
+}
+
+
 
 
--- research/iridium/iridium-9602-client/src/iridium9602.h
+++ research/iridium/iridium-9602-client/src/iridium9602.h
@@ -9,6 +9,7 @@
 #define IRIDIUM9602_H_
 
 #include <stdint.h>
+#include <stddef.h>
 
 #include "iridium9602_commands.h"
 
@@ -16,15 +17,17 @@
 /*! FIXME: 50 байт хватит ведь? */
 #define IR9602_CMD_BUFFER_SIZE (50)
 
-//! Размер буфера для Mobile Originated сообщения
-/*! MO сообщение для 9602 не может быть больше 340 байт
- *  И требует контрольную сумму в 2 байта в конце */
-#define IR9602_MO_BUFFER_SIZE  (340+2)
+//! Максимальный размер MO сообщения для 9602
+#define IR9602_MO_MSG_MAX_SIZE (340)
 
-//! Размер буфера для Mobile Terminated сообщения
-/*! MT сообщение для 9602 не может быть больше 270 байт
- *  После сообщения модем присылает два байта контрольной суммы */
-#define IR9602_MT_BUFFER_SIZE (270+2)
+//! Максимальный размер MT сообщения для 9602
+#define IR9602_MT_MSG_MAX_SIZE (279)
+
+//! Размер буфера для данных MT/MO сообщений
+/*! буфер кроме сообщения хранит 2 байта для размера сообщения
+ *  и 2 байта для контрольной суммы */
+#define IR9602_DATA_BUFFER_SIZE  (2+340+2)
+
 
 //! Указатель на функцию для чтения данных из уарта связанного с модемом
 /*! Функция должна читать сколько может без блокировки вызывающего потока,
@@ -39,27 +42,15 @@ typedef int (*ir9602_uart_read_t)(void * rwarg, void * buffer, int buffer_size);
 typedef int (*ir9602_uart_write_t)(void * rwarg, const void * buffer, int buffer_size);
 
 
-//! Дескриптор устройства 9602
-typedef struct
+// Состояния драйвера
+typedef enum
 {
-	void * rwarg;
-	ir9602_uart_read_t uart_read;
-	ir9602_uart_write_t uart_write;
-
-	ir9602_cmd_t cmd;
-	char cmd_buffer[IR9602_CMD_BUFFER_SIZE];
-	int cmd_size;
-	int cmd_carret;
-
-	uint8_t mo_buffer[IR9602_MO_BUFFER_SIZE];
-	uint16_t mo_msg_size;
-	uint16_t mo_msg_carret;
-
-	uint8_t mt_msg_buffer[IR9602_MT_BUFFER_SIZE];
-	uint16_t mt_msg_size;
-	uint16_t mt_msg_carret;
-
-} ir9602_t;
+	IR9602_STATE_IDLE,
+	IR9602_STATE_CMD,
+	IR9602_STATE_DATA_WRITE,
+	IR9602_STATE_DATA_READ,
+	IR9602_STATE_AWAIT_RESP,
+} ir9602_state_t;
 
 
 //! Результат обработки события от модема
@@ -69,11 +60,34 @@ typedef enum
 	IR9602_POLL_RESULT_NONE,
 	//! Модуль ответил каким-то неожиданным событием
 	IR9602_POLL_RESULT_EVENT,
-	//! Модуль ответил на предидущую команду
-	IR9602_POLL_RESULT_CMD_RESP,
+	//! Выполнение команды успешно завершено
+	IR9602_POLL_RESULT_CMD_COMPLETE,
 	//! Модуль ответил чем-то не понятным впринципе
-	IR9602_POLL_RESULT_ERROR,
+	IR9602_POLL_RESULT_UNEXPECTED,
 } ir9602_poll_result_t;
+
+
+
+
+//! Дескриптор устройства 9602
+typedef struct
+{
+	void * rwarg;
+	ir9602_uart_read_t uart_read;
+	ir9602_uart_write_t uart_write;
+
+	ir9602_cmd_t cmd;
+	char cmd_buffer[IR9602_CMD_BUFFER_SIZE];
+	uint16_t cmd_size;
+	uint16_t cmd_carret;
+
+	uint8_t data_buffer[IR9602_DATA_BUFFER_SIZE];
+	uint16_t data_size;
+	uint16_t data_carret;
+
+	ir9602_state_t state;
+} ir9602_t;
+
 
 
 //! Инициализация дескриптора модема
@@ -88,7 +102,22 @@ void ir9602_init(ir9602_t * device, ir9602_uart_read_t uart_read, ir9602_uart_wr
 		void * rwarg
 );
 
-//! Пробует получить и распарсить одно событие от модема
+
+//! Выполнение команды AT
+int ir9602_cmd_at(ir9602_t * device);
+
+//! Функция иницирует асинхронную операцию отправки MO сообщения на модем (при помощи команды SBDWB)
+/*! Возвращает 0 если все хорошо и -EBUSY, если в данный момент выполняется какая-то другая команда */
+int ir9602_cmd_sbdwb(ir9602_t * device, const void * buffer, int buffer_size);
+
+//! Функция иницирует асинхронную операцию получения MT сообщения с модема (SBDRB)
+/*! Возвращает 0 если все хорошо и -EBUSY, если в данный момент выполняется какая-то другая команда */
+int ir9602_cmd_sbdrb(ir9602_t * device);
+
+
+int ir9602_get_mt_data(ir9602_t * device, void * buffer, int buffer_size);
+
+//! Пробует получить распарсить одно событие от модема
 int ir9602_poll_one(ir9602_t * device);
 
 
